e3390b927ca52961e4628e1e71dcd89f46b9c793
Isaacgeo07 <isaacgeo872@gmail.com>
2026-05-16 10:19:26 +0100

    mobile update

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -13,6 +14,8 @@ const nextConfig = {
       },
     ],
   },
+
+  allowedDevOrigins: ['*'],
 }
 
 export default nextConfig
